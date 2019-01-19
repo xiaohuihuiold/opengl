342df7cbba3fa23ef6a808541b4034be68f271fb
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-01-20 00:49:01 +0800

    添加glm

--- opengl_glfw01/src/main.cpp
+++ opengl_glfw01/src/main.cpp
@@ -5,6 +5,9 @@
 #include <string>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include "shader/Shader.h"
 #include "stb_image.h"
 
