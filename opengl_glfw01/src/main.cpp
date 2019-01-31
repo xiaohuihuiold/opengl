82ddf32a582de5286b8a0993d7b0f1751bee82fc
xiaohuihuiold <xiaohuihuiold@gmail.com>
2019-01-31 21:51:59 +0800

    添加注释 添加多个立方体 添加相机 相机环绕(复制教程代码)

--- opengl_glfw01/src/main.cpp
+++ opengl_glfw01/src/main.cpp
@@ -11,9 +11,10 @@
 #include "shader/Shader.h"
 #include "stb_image.h"
 
-#define WINDOW_WIDTH 1200
-#define WINDOW_HEIGHT 800
 #define WINDOW_TITLE "OpenGL"
+
+int WINDOW_WIDTH = 1200;
+int WINDOW_HEIGHT = 800;
 
 GLfloat vertices[] = {
         -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
@@ -59,6 +60,15 @@ GLfloat vertices[] = {
         -0.5f, 0.5f, -0.5f, 0.0f, 1.0f
 };
 
+glm::vec3 cubePosition[] = {
+        glm::vec3(0.0f, 0.0f, 0.0f),
+        glm::vec3(2.0f, 3.0f, -1.0f),
+        glm::vec3(0.0f, -2.0f, 1.0f),
+        glm::vec3(3.0f, 2.0f, -2.0f),
+        glm::vec3(0.0f, -3.0f, 2.0f),
+        glm::vec3(3.0f, 3.0f, 3.0f),
+};
+
 GLfloat texCoords[] = {
         0.0f, 0.0f,
         1.0f, 1.0f,
@@ -97,29 +107,35 @@ int main() {
         return -1;
     }
 
+    // 启用深度缓冲(Z缓冲)
     glEnable(GL_DEPTH_TEST);
-    glEnable(GL_BLEND);
-    //glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    // 顶点数组对象
     GLuint VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
 
+    // 顶点缓冲对象
     GLuint VBO;
     glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    GLuint EBO;
-    glGenBuffers(1, &EBO);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
+    /* GLuint EBO;
+     glGenBuffers(1, &EBO);
+     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+ */
     Shader shader("../assets/shader/test_vertex.glsl", "../assets/shader/test_fragment.glsl");
 
+    // 模型顶点传递给顶点着色器
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) nullptr);
     glEnableVertexAttribArray(0);
 
+    // 模型材质顶点传递给着色器
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *) (3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
@@ -129,8 +145,10 @@ int main() {
     GLuint texture;
     glGenTextures(1, &texture);
     glBindTexture(GL_TEXTURE_2D, texture);
+    // 重复
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    // 线性过渡
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     int width;
@@ -142,28 +160,45 @@ int main() {
     glGenerateMipmap(GL_TEXTURE_2D);
     stbi_image_free(data);
 
-    glm::mat4 model = glm::mat4(1.0f);
-    model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-    glm::mat4 view = glm::mat4(1.0f);
-    view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+    // 相机位置向量
+    glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+    // 相机观察目标向量
+    glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+    // 相机位置减去目标向量取得相机方向,此处反向减求得正方向
+    glm::vec3 cameraDirection = glm::normalize(cameraTarget - cameraPos);
+    // 上向量
+    glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+    // 上向量与相机位置叉乘取得与他们垂直的右轴
+    glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
+    // 相机右轴与相机方向叉乘取得上轴
+    glm::vec3 cameraUp = glm::cross(cameraRight, cameraDirection);
 
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_HEIGHT / (float) WINDOW_HEIGHT, 0.1f,
+    // 透视投影矩阵
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f,
                                             100.0f);
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        // 清除颜色缓冲以及深度缓冲
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glBindTexture(GL_TEXTURE_2D, texture);
 
         shader.use();
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float) glfwGetTime() * glm::radians(-55.0f), glm::vec3(1.0f, 1.0f, 1.0f));
-        GLint modelLocation = glGetUniformLocation(shader.ID, "modelMatrix");
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        // 根据宽高比创建投影矩阵
+        projection = glm::perspective(glm::radians(45.0f), (float) WINDOW_WIDTH / (float) WINDOW_HEIGHT, 0.1f,
+                                      100.0f);
+        // 相机环绕观察
+        float radius = 10.0f;
+        float camX = (float) sin(glfwGetTime()) * radius;
+        float camY = (float) tan(glfwGetTime()) * radius;
+        float camZ = (float) cos(glfwGetTime()) * radius;
+        glm::mat4 view = glm::mat4(1.0f);
+        // lookAt矩阵 相机位置，观察目标，上向量
+        view = glm::lookAt(glm::vec3(camX, camZ, camZ), cameraTarget, up);
 
         GLint viewLocation = glGetUniformLocation(shader.ID, "viewMatrix");
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
@@ -171,9 +206,17 @@ int main() {
         GLint projectionLocation = glGetUniformLocation(shader.ID, "projectionMatrix");
         glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
 
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
         //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (int i = 0; i < 6; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePosition[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            GLint modelLocation = glGetUniformLocation(shader.ID, "modelMatrix");
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
         glfwPollEvents();
         glfwSwapBuffers(window);
@@ -181,7 +224,7 @@ int main() {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    //glDeleteBuffers(1, &EBO);
 
     glfwTerminate();
     return 0;
@@ -194,5 +237,7 @@ void processInput(GLFWwindow *window) {
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+    WINDOW_WIDTH = width;
+    WINDOW_HEIGHT = height;
     glViewport(0, 0, width, height);
 }
